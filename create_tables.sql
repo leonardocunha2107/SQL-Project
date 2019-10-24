8deab0efd78ff910c7bb10551ccce3f3db80ef63
leonardocunha2107 <leonardocunha2107@gmail.com>
2019-10-24 15:23:42 +0200

    first commit

--- create_tables.sql
+++ create_tables.sql
@@ -1,11 +1,43 @@
 
-create table type
+create table artist_type
 (
-    tid INTEGER,
-    tname VARCHAR,
-    PRIMARY KEY(tid)
+    id INTEGER,
+    name VARCHAR,
+    PRIMARY KEY(id)
 );
+CREATE TABLE gender(
+    id INTEGER,
+    name VARCHAR,
+    PRIMARY KEY(id)
+);
+CREATE TABLE contry(
+    id  INTEGER,
+    name VARCHAR,
+    PRIMARY KEY(id)
+);
+
 CREATE TABLE artist(
-    aid INTEGER,
+    id INTEGER,
+    name VARCHAR,
+    gender INTEGER,
+    sday INTEGER,
+    smonth INTEGER,
+    syear INTEGER,
+    eday INTEGER,
+    emonth INTEGER,
+    eyear INTEGER,
+    type INTEGER,
+    area VARCHAR,
+
+
+    PRIMARY KEY(name),
+    FOREIGN KEY(type) REFERENCES type.id
+
+    CHECK (0<=eday AND 31>=eday AND 0<=sday AND sday<=31 AND
+            0<=smonth AND 12>=smonth AND 0<=emonth AND 12>= emonth)
+    C
     
+
 );
+
+create ASSERTION oneGender
