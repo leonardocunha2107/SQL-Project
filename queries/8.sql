b8c32d7781e652feb75346ef162f3f0e1071c78b
Matheus Centa <matheuscenta@gmail.com>
2019-11-14 16:04:48 +0100

    Append empty line to the end of the queries

--- queries/8.sql
+++ queries/8.sql
@@ -5,7 +5,4 @@ WHERE rc.country IN (SELECT rc_after.country
 	                 WHERE (rc_before.release = rc_after.release 
                             AND (rc_before.year < rc_after.year
                                  OR (rc_before.year = rc_after.year AND rc_before.month < rc_after.month)
-                                 OR (rc_before.year = rc_after.year AND rc_before.month = rc_after.month AND rc_before.day < rc_after.day)
-                                )
-                           )
-                    );
+                                 OR (rc_before.year = rc_after.year AND rc_before.month = rc_after.month AND rc_before.day < rc_after.day))));
