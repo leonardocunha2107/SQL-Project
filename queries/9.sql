bf8dce14e42dabcfaafba025ec0b38fe28ef97b2
PostgreSQL administrator <postgres@DESKTOP-J7H63GT.localdomain>
2019-11-14 21:55:55 +0100

    9 made

--- queries/9.sql
+++ queries/9.sql
@@ -1,21 +1,27 @@
-SELECT 
-    c.name as country, 
-    a.name as artist, 
-    (SELECT COUNT(*)
-	 FROM artist older
-	 WHERE (older.area = a.area
-            AND (older.syear < a.syear
-                 OR (older.syear = a.syear AND older.smonth < a.smonth)
-                 OR (older.syear = a.syear AND older.smonth = a.smonth AND older.sday < a.sday)))
-	 LIMIT 1) as nb,
-    (SELECT COUNT(*)
-	 FROM artist older
-	 WHERE (older.syear < a.syear
-            OR (older.syear = a.syear AND older.smonth < a.smonth)
-            OR (older.syear = a.syear AND older.smonth = a.smonth AND older.sday < a.sday))
-	 LIMIT 1) as nb_global
-FROM 
-    artist a
-    INNER JOIN country c ON c.id = a.area
-    INNER JOIN artist_type atype ON atype.id = a.type
-WHERE atype.name LIKE 'Person';
+Create view acb as 
+ (SELECT c.name AS country,
+  a.name AS artist,
+  concat(lpad(a.syear::text, 4, '0'::text), '-', lpad(a.smonth::text, 2, '0'::text), '-', lpad(a.sday::text, 2,             '0'::text))::date 
+  AS borndate
+
+  FROM artist a,
+  country c
+   WHERE c.id = a.area AND a.syear IS NOT NULL AND a.smonth IS NOT NULL AND a.sday IS NOT NULL  and a.type=1);
+
+
+EXPLAIN select globcount.country, globcount.artist, loccount.nb_local, globcount.nb_global
+
+from
+ (select country, artist, ((ROW_NUMBER() OVER (ORDER BY borndate))-1) 
+   as nb_global 
+  from acb) as globcount,
+
+ (select country, artist, ((ROW_NUMBER() OVER (PARTITION BY country ORDER BY borndate))-1) 
+   as nb_local 
+  from acb) as loccount
+ 
+where globcount.country= loccount.country and globcount.artist=loccount.artist;
+
+drop view acb;
+
+
